ccfdb60ac2f5021a637f79309d5d0bb048b13ac4
James McKinney <james@slashpoundbang.com>
2018-06-08 10:36:42 -0400

    Move JSON Schema to schema/, change validate.py from a script to a test, remove ocdsextensionregistry package, move dependencies to requirements.txt. compile.py will be restored in a future commit.

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -7,19 +7,21 @@ from jsonschema import FormatChecker
 from jsonschema.validators import Draft4Validator as validator
 
 
-def validate_registry_csv(extensions_path, extension_versions_path):
-    configuration = (
+def test_registry():
+    configuration = {
         # Id must be unique in extensions file.
-        (extensions_path, 'extension-schema.json', {'Id': None}),
+        'extensions.csv': {'Id': None},
         # Version and Base URL must be unique, within the scope of a given Id, in extension versions file.
-        (extension_versions_path, 'extension-version-schema.json', {'Version': 'Id', 'Base URL': 'Id'}),
-    )
+        'extension_versions.csv': {'Version': 'Id', 'Base URL': 'Id'},
+    }
 
     # Keep track of extension identifiers, to ensure consistency across files.
     identifiers = {}
 
-    for data_file, schema_file, uniqueness in configuration:
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), schema_file)) as f:
+    for data_basename, uniqueness in configuration.items():
+        schema_basename = '{}-schema.json'.format(os.path.splitext(data_basename)[0])
+
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'schema', schema_basename)) as f:
             schema = json.load(f)
 
         # Count the occurrences of a key-value pair, within a given scope.
@@ -30,7 +32,7 @@ def validate_registry_csv(extensions_path, extension_versions_path):
             else:
                 seen[key] = set()
 
-        with open(data_file) as f:
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', data_basename)) as f:
             reader = csv.DictReader(f)
             for row in reader:
                 id = row['Id']
@@ -48,15 +50,15 @@ def validate_registry_csv(extensions_path, extension_versions_path):
                     if scope:
                         if value in seen[scope][row[scope]][key]:
                             raise Exception('{}: Duplicate {} "{}" in scope of {} "{}" on line {}'.format(
-                                data_file, key, value, scope, row[scope], reader.line_num))
+                                data_basename, key, value, scope, row[scope], reader.line_num))
                         seen[scope][row[scope]][key].add(value)
                     else:
                         if value in seen[key]:
                             raise Exception('{}: Duplicate {} "{}" on line {}'.format(
-                                data_file, key, value, reader.line_num))
+                                data_basename, key, value, reader.line_num))
                         seen[key].add(value)
 
-                if schema_file == 'extension-schema.json':
+                if data_basename == 'extensions.csv':
                     identifiers[id] = 0
                 else:
                     # Ensure every version belongs to a known extension.
--- validate.py
+++
@@ -1,5 +0,0 @@
-import ocdsextensionregistry.validate
-import os
-
-if __name__ == "__main__":
-    ocdsextensionregistry.validate.validate_registry_csv(os.path.dirname(os.path.abspath(__file__)) + '/extensions.csv')
