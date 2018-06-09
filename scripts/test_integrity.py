de89f21c31b677aea59662b8c2bdfba80f4e5b74
James McKinney <james@slashpoundbang.com>
2018-06-08 21:05:52 -0400

    Add build/extension_versions_wide.csv, for use cases in which data from both extensions.csv and extension_versions.csv is required

--- scripts/test_integrity.py
+++ scripts/test_integrity.py
@@ -5,13 +5,22 @@ Ensures that `compile.py` has been run.
 import logging
 import os
 
-from .compile import compile_registry
+from .compile import compile_extensions_js, compile_extension_versions_wide_csv
+
+directory = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_extensions_js_is_in_sync(caplog):
+def compare(caplog, basename, expected):
     caplog.set_level(logging.INFO)  # silence connectionpool.py DEBUG messages
 
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'build', 'extensions.js')) as f:
+    with open(os.path.join(directory, '..', 'build', basename)) as f:
         actual = f.read()
 
-    assert actual == compile_registry()
+    assert actual == expected
+
+def test_extensions_js_is_in_sync(caplog):
+    compare(caplog, 'extensions.js', compile_extensions_js())
+
+
+def test_extension_versions_wide_csv_is_in_sync(caplog):
+    compare(caplog, 'extension_versions_wide.csv', compile_extension_versions_wide_csv())
