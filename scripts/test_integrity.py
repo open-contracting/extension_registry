c2ddcc73c5e6cf16938c6db474a180b5569d05f5
James McKinney <james@slashpoundbang.com>
2018-06-12 17:07:09 -0400

    Use JSON instead of JS file, to avoid an issue relating to X-Content-Type-Options: nosniff

--- scripts/test_integrity.py
+++ scripts/test_integrity.py
@@ -5,13 +5,13 @@ Ensures that `compile.py` has been run.
 import logging
 import os
 
-from .compile import compile_extensions_js
+from .compile import compile_extensions_json
 
 
 def test_extensions_js_is_in_sync(caplog):
     caplog.set_level(logging.INFO)  # silence connectionpool.py DEBUG messages
 
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'build', 'extensions.js')) as f:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'build', 'extensions.json')) as f:
         actual = f.read()
 
-    assert actual == compile_extensions_js()
+    assert actual == compile_extensions_json()
