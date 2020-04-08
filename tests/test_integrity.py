df61612dccaf99e70fc2cd20e0dd3cd439df8037
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-08 16:26:59 -0400

    Fix test_integrity.py

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -5,13 +5,13 @@ Ensures that `compile.py` has been run.
 import logging
 import os
 
-from .compile import compile_extensions_json
+from manage import do_compile
 
 
-def test_extensions_js_is_in_sync(caplog):
+def test_build(caplog):
     caplog.set_level(logging.INFO)  # silence connectionpool.py DEBUG messages
 
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'build', 'extensions.json')) as f:
         actual = f.read()
 
-    assert actual == compile_extensions_json()
+    assert actual == do_compile()
